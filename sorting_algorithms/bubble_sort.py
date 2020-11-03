cb2840cb0ae88f5c05a35563209a52bedd782630
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-03 02:24:02 +0100

    Create bubble_sort.py

---
+++ sorting_algorithms/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [2, 5, 3, 1, 2, 6]
+bubble_sort(arr)
+print(arr)
