c7bc62df7b556392f54dd4df325100667c6de1af
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-03 02:38:13 +0100

    Update bubble_sort.py
    Add optimized version

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -6,6 +6,21 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
+def optimized_bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if swapped == False:
+            break
+
+
 arr = [2, 5, 3, 1, 2, 6]
+second_array = [2, 5, 3, 1, 2, 6]
 bubble_sort(arr)
+optimized_bubble_sort(second_array)
 print(arr)
+print(second_array)
